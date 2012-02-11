5c4ba6b900f652606abf2a4d221cea3ee8a10c6f
Anthony Navarre <me@anthonynavarre.com>
2012-02-11 10:32:31 -0700

    Implementation for single digit, test for 2 digits

--- spec/string_counter/string_counter_spec.rb
+++ spec/string_counter/string_counter_spec.rb
@@ -11,10 +11,16 @@ describe 'String Calculator' do
     context "a string with a single number" do
 
       it "returns the number" do
-        3.times do |i|
-          i.to_s.add.should == i
-        end
+        '1'.add.should == 1
       end
+    end
+
+    context 'when dealing with more than 1 digit' do
+
+      it 'returns the sum of its parts' do
+        '1,2'.add.should == 3
+      end
+
     end
 
   end
