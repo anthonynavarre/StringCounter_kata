db0002f26e7b5e129c75e839f52ce38cf91a2f03
Sean Luckett <sean.luckett@colorado.edu>
2012-02-15 10:44:09 -0700

    Separates finding negatives from summing in string_counter.

--- lib/string_counter.rb
+++ lib/string_counter.rb
@@ -1,13 +1,21 @@
 class String
   def add
     self.gsub!(/[^-|\d]/, ',')
-    
+    numbers = self.split(',')
+
+    find_negatives(numbers)
+
+    numbers.inject(0) do |sum, num|
+      sum + num.to_i
+    end
+  end
+
+  def find_negatives(numbers)
     negatives = []
-    sum = self.split(',').inject(0) do |sum, num|
-      negatives.push(num.to_i) if i < 0
-      sum + i
+    numbers.each do |num|
+      n = num.to_i
+      negatives.push(n) if n < 0
     end
     raise ArgumentError.new("Negatives not allowed. Passed negatives: #{negatives.join(',')}") if negatives.any?
-    sum
   end
 end
