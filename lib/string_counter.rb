d9d88b3f809621ebc7e310c2b5aa62618db3adab
Sean Luckett <sean.luckett@colorado.edu>
2012-02-15 10:16:05 -0700

    Refactors conditional in #add; replaced with one line

--- lib/string_counter.rb
+++ lib/string_counter.rb
@@ -1,15 +1,10 @@
 class String
   def add
-    if self =~ %r(^//(.)\n)
-      delimiter = $1
-    else
-      delimiter = /\n|,/
-    end
-
+    self.gsub!(/[^-|\d]/, ',')
+    
     negatives = []
-    sum = self.split(delimiter).inject(0) do |sum, num|
-      i = num.to_i
-      negatives.push(i) if i < 0
+    sum = self.split(',').inject(0) do |sum, num|
+      negatives.push(num.to_i) if i < 0
       sum + i
     end
     raise ArgumentError.new("Negatives not allowed. Passed negatives: #{negatives.join(',')}") if negatives.any?
